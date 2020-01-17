e3dd4487d2134942aa87775b8bc9d1921c531f75
Noel Röhrig <32901324+idmedia-roehrig@users.noreply.github.com>
2020-01-17 11:35:51 +0100

    Cleanup of repo with .gitignore file

--- code/DisplayDepartures.py
+++ code/DisplayDepartures.py
@@ -11,11 +11,11 @@ def UpdateDepartures():
     for tripModel in _tripListModel.Trips[:5]:
         countdown = tripModel.departure - datetime.now(timezone.utc)
         try:
-            app.setLabel(f"{tripModel.tripId}-departure", countdown.total_seconds() // 60)
+            app.setLabel(f"{tripModel.tripId}-departure", f"{'{0:.1f}'.format(countdown.total_seconds() / 60)} min")
         except appjar.ItemLookupError:
             row = app.getRow()
             app.addLabel(tripModel.tripId, tripModel.direction, row, 0)
-            app.addLabel(f"{tripModel.tripId}-departure", countdown.total_seconds() // 60, row, 1)
+            app.addLabel(f"{tripModel.tripId}-departure", f"{'{0:.1f}'.format(countdown.total_seconds() / 60)} min", row, 1)
         if(countdown.total_seconds() < 0):
             app.removeLabel(tripModel.tripId)
             app.removeLabel(f"{tripModel.tripId}-departure")
@@ -29,7 +29,7 @@ def PrintDepartures():
         
         row = app.getRow()
         app.addLabel(tripModel.tripId, tripModel.direction, row, 0)
-        app.addLabel(f"{tripModel.tripId}-departure", countdown.total_seconds() // 60, row, 1)
+        app.addLabel(f"{tripModel.tripId}-departure", f"{'{0:.1f}'.format(countdown.total_seconds() / 60)} min", row, 1)
 
     # start the GUI
     app.registerEvent(UpdateDepartures)
